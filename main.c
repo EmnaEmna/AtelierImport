9a5427161d185e1b1deb88f396dcd300285b49b4
EmnaEmna <emna.meknii@gmail.com>
2021-11-03 17:25:42 +0100

    mise à jour main

--- main.c
+++ main.c
@@ -5,11 +5,8 @@ int a;
 int b;
 printf ("\nSaisie du Premier Nombre: ");
 scanf("%d",& a);
-do { 
+
 printf ("\nSaisie du second Nombre: ");
 scanf("%d",& b);
-}
-while (b==0);
-printf("\n %d + %d = %d", a, b,a+b);
-return (0);
+
 }
